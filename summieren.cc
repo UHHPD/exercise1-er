366d6f39016559bd294ee05df9f2f90f9b1e011e
W-A-I-D <>
2021-10-19 14:15:12 +0000

    Ausgabe 2

---
+++ summieren.cc
@@ -0,0 +1,20 @@
+git #include <fstream>
+#include <iostream>
+
+
+int main() 
+{
+  std::ifstream fin("daten.txt");
+  std::ofstream fout("datensumme.txt");
+  int a, b, c;
+  while(! fin.eof()) 
+  {
+    fin >> a >> b;
+    c = a + b;
+    // std::cout << "Die Summe aus " << a << " und " << b << " ist " << c << std::endl;
+    fout << c << std::endl;
+  }
+  fin.close();
+  fout.close();
+}
+
