a589991437c5aef18bd71f62f0190fabd0d7e333
W-A-I-D <>
2021-10-27 13:28:14 +0000

    Update Blatt 2

--- summieren.cc
+++ summieren.cc
@@ -7,6 +7,13 @@ int main()
   std::ifstream fin("daten.txt");
   std::ofstream fout("datensumme.txt");
   int a, b, c;
+  for(int i = 0; i<234; ++i) 
+  {
+  fin >> a >> b;
+  c=a+b;
+  fout << c << std::endl;
+  }
+/*  
   while(! fin.eof()) 
   {
     fin >> a >> b;
@@ -14,6 +21,7 @@ int main()
     // std::cout << "Die Summe aus " << a << " und " << b << " ist " << c << std::endl;
     fout << c << std::endl;
   }
+  */
   fin.close();
   fout.close();
 }
