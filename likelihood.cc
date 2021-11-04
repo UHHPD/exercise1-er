8b6691868b4eca5083fc359f02c243eb5635c0c1
W-A-I-D <>
2021-11-04 19:08:07 +0000

    Blatt 3

---
+++ likelihood.cc
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<vector>
+
+int Ntot=234;
+
+using namespace std;
+vector<int> daten(Ntot);
+
+/*
+Definiere Poisson-Funktion.
+Argumente sind Mittelwert mu und Häufigkeit k.
+Ausgabe ist Wahrscheinlichkeit, die Häufigkeit k zu beobachten
+*/
+double poisson(double mu, int k) {
+  double prob = pow(mu,k)*exp(-mu)/(tgamma(k+1));
+  return prob;
+}
+
+/*
+Definiere Likelihood-Funktion.
+Argumente sind der Mittelwert mu und der Vektor, der das beobachtete Sample enthält.
+Ausgabe ist Wahrscheinlichkeit, genau das gegebene Sample bei gegebenem Mittelwert zu beobachten.
+Hierzu multipliziert die Funktion die Poisson-Wahrscheinlichkeit für alle Events miteinander.
+*/
+
+double prob(std::vector<int> daten, double mu) {
+  double sprob = 1;
+  for(int k : daten) {
+    double kprob = poisson(mu,k);
+    sprob *= kprob;
+    //std::cout << m << " " << daten[m] <<  " " << k << " " << kprob << " " << sprob << std::endl;
+  }
+  return sprob;
+}
+
+int main() {
+
+ifstream fin("datensumme.txt");
+ofstream fout("likelihood.txt");
+
+//Fülle Vektor "daten" mit den Werten aus datensumme.txt
+//Die push_back-Funktion hängt den ihr gegebenen Wert an den Vektor an.
+int zahl = 0;
+for(int i = 0; i < Ntot; i++) {
+  fin >> zahl;
+  daten[i] = zahl;
+  //daten.push_back(zahl);
+  //std::cout << zahl << std::endl;
+}
+
+fin.close();
+
+/*
+for(int m = 0; m < daten.size(); m++) {
+    double k = daten[m];
+    std::cout << m << " " << daten[m] <<  " " << k << std::endl;
+  }
+*/
+
+std::cout << prob(daten, 3.11538) << std::endl;
+
+/*
+Schreibe die Werte von mu und L(mu) für Werte von mu zwischen 0 und 6 in Intervallen von 0,1 in die Datei likelihood.txt.
+L(mu) basiert auf dem beobachteten Sample.
+*/
+
+for (int j = 0; j < 61; j++) {
+  double mu1 = 0.1 * j;
+  double muprob = prob(daten, mu1);
+  //Reicht das so als Input für die Funktion prob? Muss die ganze Vektordefinition angegeben werden?
+  fout << mu1 << " " << muprob << std::endl;
+}
+
+fout.close();
+
+ofstream fout2("nll.txt");
+
+/*
+Schreibe die Werte von mu und - 2 ln (L(mu)) für Werte von mu zwischen 0 und 6 in Intervallen von 0,1 in die Datei nll.txt.
+L(mu) basiert auf dem beobachteten Sample.
+*/
+
+for (int j = 0; j < 61; j++) {
+  double mu1 = 0.1 * j;
+  double muprob = prob(daten, mu1);
+  double nll = - 2 * log(muprob);
+  //Reicht das so als Input für die Funktion prob? Muss die ganze Vektordefinition angegeben werden?
+  fout2 << mu1 << " " << nll << std::endl;
+}
+
+
+
+}
---
+++ poisson.cc
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<vector>
+
+/*
+Definiere Poisson-Funktion.
+Argumente sind Mittelwert mu und Häufigkeit k.
+Ausgabe ist Wahrscheinlichkeit, die Häufigkeit k zu beobachten
+*/
+double poisson(double mu, int k) {
+  double prob = pow(mu,k)*exp(-mu)/(tgamma(k+1));
+  return prob;
+}
+
+int main() {
+
+using namespace std;
+vector<int> zaehler(11);
+
+int Ntot=234;
+
+ifstream fin("datensumme.txt");
+
+//Fülle Zähler-Vektor
+int zahl=0;
+for(int i = 0 ; i < Ntot; i++) {
+  fin >> zahl;
+  zaehler[zahl] += 1;
+}
+
+fin.close();
+
+//Ausgabe des Wertes von k und des zugehörigen Eintrags des Vektors
+for(unsigned int k = 0; k < zaehler.size(); ++k) {
+  std::cout << k << ": " << zaehler[k] << std::endl;
+}
+
+ofstream fout("hist.txt");
+
+//Schreibe Inhalt des Vektors in die Datei hist.txt
+for(unsigned int k = 0; k < zaehler.size(); ++k) {
+  fout << k << " " << zaehler[k] << std::endl;
+}
+
+fout.close();
+
+ofstream fout2("histpoi.txt");
+
+/*
+Ausgabe des Wertes von k, des zugehörigen Eintrags des Zähler-Vektors und der erwarteten Häufigkeit bei der Ereignisanzahl.
+Die Werte werden in die Datei histpoi.txt geschrieben
+Die erwartete Häufigkeit resultiert aus der Wahrscheinlichkeit aus der Poisson-Verteilung.
+*/
+for(unsigned int m = 0; m < zaehler.size(); ++m) {
+  double pevents = poisson(3.11538, m) * Ntot;
+  fout2 << m << " " << zaehler[m] << " " << pevents << std::endl;
+}
+
+fout2.close();
+
+}
